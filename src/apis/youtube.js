3aadf669ed738a0704db17d559ba9e2f0a3ed4dc
woejaddicor <joe4091@googlemail.com>
2022-03-17 17:01:55 +0000

    Added readme changes

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import key from './key';
 
-const KEY = key;
-
 export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
 	params: {
--- src/components/App.js
+++ src/components/App.js
@@ -5,39 +5,39 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
+	const [videos, setVideos] = useState([]);
+	const [selectedVideo, setSelectedVideo] = useState(null);
 
-  useEffect(() => {
-    onTermSubmit('buildings');
-  }, []);
+	useEffect(() => {
+		onTermSubmit('lfc milan champions league final');
+	}, []);
 
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term,
-      },
-    });
+	const onTermSubmit = async (term) => {
+		const response = await youtube.get('/search', {
+			params: {
+				q: term,
+			},
+		});
 
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+		setVideos(response.data.items);
+		setSelectedVideo(response.data.items[0]);
+	};
 
-  return (
-    <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
-      <div className="ui grid">
-        <div className="ui row">
-          <div className="eleven wide column">
-            <VideoDetail video={selectedVideo} />
-          </div>
-          <div className="five wide column">
-            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="ui container">
+			<SearchBar onFormSubmit={onTermSubmit} />
+			<div className="ui grid">
+				<div className="ui row">
+					<div className="eleven wide column">
+						<VideoDetail video={selectedVideo} />
+					</div>
+					<div className="five wide column">
+						<VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
